aac86db28629ad4f0c4cb527162cc9ca879701d6
Paul Drury <paul@drurys.org>
2022-06-27 14:13:55 +0200

    Home Page displays properly

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './ui/general/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    // landing pages
+    { path: "home", component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
