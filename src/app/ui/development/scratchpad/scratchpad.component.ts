01bfc10bcc30815ccc5c8097103e48065e4c16ea
Paul <paul@drurys.org>
2024-12-31 14:30:40 +0000

    Added some debug

--- src/app/ui/development/scratchpad/scratchpad.component.ts
+++ src/app/ui/development/scratchpad/scratchpad.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { IActivePuzzle } from 'src/app/services/puzzles/puzzle-management.service';
 
 @Component({
   selector: 'app-puzzle-scratchpad',
@@ -8,25 +8,47 @@ import { IActivePuzzle } from 'src/app/services/puzzles/puzzle-management.servic
   styleUrls: ['./scratchpad.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScratchpadComponent implements OnInit, OnDestroy {
+export class ScratchpadComponent implements OnInit, OnDestroy, AfterViewInit {
     private subs: Subscription[] = [];
 
-    public hideSuggestions = false;
+    @ViewChild('gridCanvas', { static: false }) 
+    public canvas: ElementRef;
+
+    public text: string = "wating for canvas";
+    public form = this.formBuilder.group({
+        mimeType: ""
+    });
 
     constructor(
-        private activePuzzle: IActivePuzzle,
         private detRef: ChangeDetectorRef,
+        private formBuilder: FormBuilder,
     ) { }
 
-    ngOnInit() {
+    ngOnInit(): void {
+        
+    }
+
+    ngAfterViewInit() {
+        const canvas: HTMLCanvasElement = this.canvas.nativeElement;
+        const context = canvas.getContext('2d');
+        context.fillStyle = "blue";
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        context.fillStyle = "yellow";
+        context.fillRect(20, 20, 30, 70);
+
+        this.detRef.detectChanges();
     }
 
     ngOnDestroy() {
         this.subs.forEach(s => s.unsubscribe())
     }
 
-    public onIgnoreSuggestions() {
-        this.hideSuggestions = true;
-    }
+    public createImage() {
+        const canvas: HTMLCanvasElement = this.canvas.nativeElement;
+        const data = canvas.toDataURL(this.form.value.mimeType);
 
+        this.text = data.substring(0, 100);
+
+        this.detRef.detectChanges();
+    }
 }
