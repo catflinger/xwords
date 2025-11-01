8e2a50881fac18f82a97398417e5f908c37ce948
Paul <paul@drurys.org>
2025-11-01 09:09:24 +0000

    Improved PDF guide

--- src/app/ui/general/guides/pdf-download-guide/pdf-download-guide.component.ts
+++ src/app/ui/general/guides/pdf-download-guide/pdf-download-guide.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NavService } from 'src/app/services/navigation/nav.service';
+import { AppTrackData } from 'src/app/services/navigation/tracks/app-track-data';
+import { AppService } from '../../app.service';
+import { IActivePuzzle } from 'src/app/services/puzzles/puzzle-management.service';
 
 @Component({
-  selector: 'app-pdf-download-guide',
-  templateUrl: './pdf-download-guide.component.html',
-  styleUrls: ['./pdf-download-guide.component.css']
+    selector: 'app-pdf-download-guide',
+    templateUrl: './pdf-download-guide.component.html',
+    styleUrls: ['./pdf-download-guide.component.css']
 })
 export class PdfDownloadGuideComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private navService: NavService<AppTrackData>,
+        private appService: AppService,
+        private activePuzzle: IActivePuzzle
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    public onLoadPDF() {
+        this.activePuzzle.clear();
+        this.appService.clear();
+        this.appService.setOpenPuzzleParams({
+            provider: "pdf",
+        });
+        this.navService.beginTrack("createPdfTrack", {});
+    }
 
 }
